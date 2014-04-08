fc52f7dba30a91931aa5c759266feeaf96b9cdbc
shengqh <shengqh@gmail.com>
2014-04-08 16:14:24 -0500

    shengqh

---
+++ Extension/GenericCollectionExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace System
+{
+  public static class GenericCollectionExtension
+  {
+    public static void ForEach<T>(this T[] items, Action<T> action)
+    {
+      foreach (var item in items)
+      {
+        action(item);
+      }
+    }
+
+    public static void ForEach<T, V>(this Dictionary<T, V> items, Action<KeyValuePair<T, V>> action)
+    {
+      foreach (var item in items)
+      {
+        action(item);
+      }
+    }
+  }
+}
