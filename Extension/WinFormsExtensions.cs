b2c2d2e11a707429bddd683a5a89891e3ee4cf09
Quanhu Sheng <quanhu.sheng@vanderbilt.edu>
2015-09-04 15:45:40 -0500

    v3.9.9

--- Extension/WinFormsExtensions.cs
+++ Extension/WinFormsExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace System.Windows.Forms
@@ -31,6 +32,12 @@ namespace System.Windows.Forms
     public static void AppendColorLine(this RichTextBox box, Color color, string format, params object[] values)
     {
       AppendLine(box, string.Format(format, values), color);
+    }
+
+    public static void DoubleBuffered(this Control control, bool enable)
+    {
+      var doubleBufferPropertyInfo = control.GetType().GetProperty("DoubleBuffered", BindingFlags.Instance | BindingFlags.NonPublic);
+      doubleBufferPropertyInfo.SetValue(control, enable, null);
     }
   }
 }
