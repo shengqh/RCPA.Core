fadfc568388b06d9d4f9b62c7b361348dc5ea3e3
Quanhu Sheng <quanhu.sheng@vanderbilt.edu>
2015-11-01 19:48:01 -0600

    add rutils

---
+++ R/RUtils.cs
@@ -0,0 +1,45 @@
+﻿using RCPA.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCPA.R
+{
+  public static class RUtils
+  {
+    public static void PerformRByTemplate(string rfile, string targetrfile, List<string> definitions)
+    {
+      using (var sw = new StreamWriter(targetrfile))
+      {
+        sw.NewLine = "\n";
+        foreach (var def in definitions)
+        {
+          sw.WriteLine(def);
+        }
+        string line = File.ReadAllText(rfile);
+        using (var sr = new StreamReader(rfile))
+        {
+          if (line.Contains("#predefine_end"))
+          {
+            while ((line = sr.ReadLine()) != null)
+            {
+              if (line.Contains("#predefine_end"))
+              {
+                break;
+              }
+            }
+          }
+
+          while ((line = sr.ReadLine()) != null)
+          {
+            sw.WriteLine(line);
+          }
+        }
+      }
+      SystemUtils.Execute("R", "--vanilla --slave -f \"" + targetrfile + "\"");
+    }
+  }
+}
